7d4ea1ade60287c5189139985723ac5507d1fcf3
Dest1n640 <realmadridfunca@gmail.com>
2026-06-29 11:32:23 +0800

    feat: add database creation in main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 import os
-
 from fastapi import FastAPI
+from app.db.database import engine, Base
+from app.db import models
+
+os.makedirs("./data", exist_ok=True)
+
+Base.metadata.create_all(bind=engine)
 
 from app.db.database import Base, engine
 
