37f126ce38d0d44b4eca1cb0bccb92727e2b1fda
Dest1n640 <realmadridfunca@gmail.com>
2026-06-29 14:06:42 +0800

    format: use ruff check fix and ruff format

--- app/schemas/evidence.py
+++ app/schemas/evidence.py
@@ -1,41 +1,48 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-from typing import Optional, Dict, Any
 import uuid
+from datetime import datetime
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, Field
+
 
 class Account(BaseModel):
-  name: str = Field(..., min_length=1, description="Имя студента")
+    name: str = Field(..., min_length=1, description="Имя студента")
+
 
 class Actor(BaseModel):
-  account: Account
+    account: Account
+
 
 class Verb(BaseModel):
-  id: str = Field(..., min_length=1, description="URL, действие")
+    id: str = Field(..., min_length=1, description="URL, действие")
+
 
 class Definition(BaseModel):
-  type: str = Field(..., min_length=1, description="Тип объекта")
+    type: str = Field(..., min_length=1, description="Тип объекта")
+
 
 class ObjectBase(BaseModel):
-  id: str = Field(..., min_length=1, description="Ссылка на артефакт (URL)")
-  definition: Definition
+    id: str = Field(..., min_length=1, description="Ссылка на артефакт (URL)")
+    definition: Definition
 
 
 class XAPIStatement(BaseModel):
-  """
-  Главная схема запроса
-  """
-  actor: Actor
-  verb: Verb
-  object: ObjectBase
-  context = Optional[Dict[str, Any]] = None
-  timestamp = datetime
+    """
+    Главная схема запроса
+    """
 
+    actor: Actor
+    verb: Verb
+    object: ObjectBase
+    context = Optional[Dict[str, Any]] = None
+    timestamp = datetime
 
 
 class EvidenceResponce(BaseModel):
     """
     Схема для отдачи данных из базы данных - наружу.
     """
+
     id: uuid.UUID
     actor_id: str
     source_system: str
@@ -48,6 +55,6 @@ class EvidenceResponce(BaseModel):
     created_at: datetime
 
     class Config:
-        # Эта настройка говорит Pydantic, что данные будут приходить 
-        # не как словари, а как ORM-объекты SQLAlchemy. 
+        # Эта настройка говорит Pydantic, что данные будут приходить
+        # не как словари, а как ORM-объекты SQLAlchemy.
         from_attributes = True
