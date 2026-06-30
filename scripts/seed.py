9af3994a8e9566390b572ac83d79ea191a946978
Dest1n640 <realmadridfunca@gmail.com>
2026-06-30 09:16:53 +0800

    feat: rewrite data generator script for new database structor

--- scripts/seed.py
+++ scripts/seed.py
@@ -1,8 +1,10 @@
 import random
 from datetime import datetime, timezone
 
+# Eсли запускаешь не из корня, а прямо из папки scripts
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.db.database import Base, SessionLocal, engine
-from app.db.models import EvidenceRecord
+from app.db.models import EvidenceRecord, ReviewStatus
 from app.schemas.evidence import XAPIStatement
 
 
@@ -11,11 +13,11 @@ def seed_data():
     db = SessionLocal()
 
     if db.query(EvidenceRecord).first():
-        print("База данных уже содержит записи.")
+        print("База данных уже содержит записи")
         db.close()
         return
 
-    statuses = ["pending", "reviewed", "rejected"]
+    statuses = [ReviewStatus.pending, ReviewStatus.reviewed, ReviewStatus.rejected]
 
     for i in range(1, 16):
         raw_data = {
@@ -25,35 +27,38 @@ def seed_data():
                 "id": f"http://github.com/org/repo/commit/mock_commit_{i}",
                 "definition": {"type": "commit"},
             },
-            "context": {"project": "Evidence Locker Test"},
+            "context": {"project": f"project_{random.randint(1, 3)}"},
             "timestamp": datetime.now(timezone.utc).isoformat(),
         }
 
         try:
             statement = XAPIStatement(**raw_data)
         except Exception as e:
-            print(f"Ошибка валидации Pydantic {i}: {e}")
+            print(f"Ошибка валидации Pydantic для записи {i}: {e}")
             continue
 
         status = random.choice(statuses)
-        reviewed_by = (
-            "0" if status in ["reviewed", "rejected"] else "0"
-        )  # По ТЗ пока всегда 0
+
+        context_dict = statement.context or {}
+        context_id = context_dict.get("project", "default_context")
 
         db_record = EvidenceRecord(
             actor_id=statement.actor.account.name,
+            verb_id=statement.verb.id,
+            object_id=statement.object.id,
+            timestamp=statement.timestamp,
             source_system="seed_script",
             source_type=statement.object.definition.type,
-            evidence_link=statement.object.id,
-            context=str(statement.context),
-            timestamp=statement.timestamp,
+            context_id=context_id,
+            note="Сгенерировано автоматически (Seed script)",
+            raw_data=raw_data,
             review_status=status,
-            reviewed_by=reviewed_by,
+            reviewed_by="0",
         )
         db.add(db_record)
 
     db.commit()
-    print("Генерация завершенна")
+    print("Успешно сгенерировано")
     db.close()
 
 
